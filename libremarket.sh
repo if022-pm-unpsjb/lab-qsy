1562e15a78f9cd028bab7fcaa3d6ba1351c4c9e9
macasteglione <matiascasteglione2002@gmail.com>
2025-09-17 20:34:47 -0300

    agregue libcluster, y modifique docker-compose, iex.sh y el supervisor

--- libremarket.sh
+++ libremarket.sh
@@ -19,6 +19,8 @@ if [[ $1 == "start" ]]; then
     start "$@"
 elif [[ $1 == "stop" ]]; then
     stop
+elif [[ $1 == "build" ]]; then
+    docker run -it --rm -v "$(pwd)":/app -w /app -u $(id -u):$(id -g) -e MIX_HOME=/app/mix_home -e HEX_HOME=/app/hex_home --network host elixir:alpine mix compile
 elif [[ $1 == "iex" ]]; then
     docker attach $2
 else
